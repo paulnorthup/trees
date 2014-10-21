9f015f5e11f91094d2225786580fae384d3b004a
Paul <paul@paulnorthup.com>
2014-10-21 16:13:24 -0400

    scaffold for gulp project

---
+++ gulpfile.js
@@ -0,0 +1,55 @@
+// Modules
+var gulp = require('gulp'),
+    slim = require('gulp-slim'),
+    sass = require('gulp-ruby-sass'),
+    plumber = require('gulp-plumber'),
+    browserSync = require('browser-sync');
+
+// Helpers
+var reload = browserSync.reload;
+
+//Paths
+var src = {
+  'css': './lib/styles/**/*.scss',
+  'js': './lib/js/**/*.js',
+  'html':'./lib/templates/*.slim'
+};
+var dest = {
+  'css': './dist/css/',
+  'js': './dist/js/',
+  'html':'./dist'
+};
+
+// Process sass
+gulp.task('sass', function() {
+  gulp.src(src.css)
+  .pipe(plumber())
+  .pipe(sass())
+  .on('error', function (err) { console.log(err.message); })
+  .pipe(gulp.dest(dest.css))
+  .pipe(reload({stream:true}));
+});
+
+// Process templates
+gulp.task('slim', function() {
+  gulp.src(src.html)
+  .pipe(plumber())
+  .pipe(slim())
+  .pipe(gulp.dest(dest.html))
+  .pipe(reload({stream:true}));
+});
+
+// Spin up a server
+gulp.task('serve', function() {
+browserSync({
+  server: {
+    baseDir: './dist'
+    }
+  });
+});
+
+// Default to build, serve, and watch
+gulp.task('default', ['sass', 'slim', 'serve'], function() {
+  gulp.watch(src.css, ['sass']);
+  gulp.watch(src.html, ['slim']);
+});
